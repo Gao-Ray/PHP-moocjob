a73b0e9a31403da33b076f54168eb1778fc619c3
gaolei <827163219@qq.com>
2019-05-15 14:40:58 +0800

    fix

--- 2-1 PHP引用变量考察点（上）/demo1.php
+++ 2-1 PHP引用变量考察点（上）/demo1.php
@@ -5,7 +5,7 @@ $a = range(0, 1000);
 var_dump(memory_get_usage());
 
 // 定义变量b，将a变量的值赋给b
-// COW Copy On Write
+// COW Copy On Write（对$a或$b有修改操作，才会进行copy）
 $b = $a;
 var_dump(memory_get_usage());
 
--- 2-1 PHP引用变量考察点（上）/demo2.php
+++ 2-1 PHP引用变量考察点（上）/demo2.php
@@ -5,7 +5,6 @@ $a = range(0, 1000);
 var_dump(memory_get_usage());
 
 // 定义变量b，将a变量的值赋给b
-// COW Copy On Write
 $b = &$a;
 var_dump(memory_get_usage());
 
